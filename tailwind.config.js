9cb42b4216e3bd2af23db9f1b6fe30a1f5a65b02
Ayomikun <ayoadeosun10@gmail.com>
2025-03-01 11:35:13 +0100

    style: added entrance animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,22 @@ export default {
       primary: ["Poppins, sans-serif"],
       head: ["Charm"],
     },
-    extend: {},
+    extend: {
+      keyframes: {
+        fadeIn: {
+          "0%": { opacity: "0", transform: "translateY(25px)" },
+          "100%": { opacity: "1", transform: "translateY(0)" },
+        },
+        textIn: {
+          "0%": { opacity: "0", transform: "translateX(20px)" },
+          "100%": { opacity: "1", transform: "translateX(0)" },
+        },
+      },
+      animation: {
+        fadeIn: "fadeIn 1s ease-out",
+        textIn: "textIn 1s",
+      },
+    },
   },
   plugins: [],
 };
